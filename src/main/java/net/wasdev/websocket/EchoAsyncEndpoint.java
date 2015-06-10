ccdc89b1037822f86c82b17966b77f52caea25e0
Erin Schnabel <ebullient@users.noreply.github.com>
2015-06-10 19:17:10 -0400

    maven build, git ignore

--- src/main/java/net/wasdev/websocket/EchoAsyncEndpoint.java
+++ src/main/java/net/wasdev/websocket/EchoAsyncEndpoint.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package net.wasdev.websocket;
 
 import java.io.IOException;
@@ -16,28 +31,30 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value = "/EchoAsyncEndpoint")
 public class EchoAsyncEndpoint {
 
+
+    /** CDI injection of Java EE7 Managed executor service */
     @Resource
     ManagedExecutorService executor;
 
     @OnOpen
     public void onOpen(Session session, EndpointConfig ec) {
     }
-    
+
     @OnClose
     public void onClose(Session session, CloseReason reason) {
     }
 
     int count = 0;
-    
+
     @OnMessage
     public void receiveMessage(final String message, final Session session) throws IOException {
         if ( "stop".equals(message) ) {
             session.close();
-        } else {            
+        } else {
             System.out.println(message + ", " + executor);
             final int id = count++;
             broadcast(session, "Echo " + id + ": " + message);
-            
+
             executor.submit(new Runnable() {
                 @Override
                 public void run() {
@@ -51,7 +68,7 @@ public class EchoAsyncEndpoint {
             });
         }
     }
-    
+
     private void broadcast(Session session, String message) {
         for (Session s : session.getOpenSessions() ) {
             try {
@@ -64,7 +81,7 @@ public class EchoAsyncEndpoint {
             }
         }
     }
-    
+
     @OnError
     public void onError(Throwable t) {
     }
--- src/main/java/net/wasdev/websocket/EchoEncoderEndpoint.java
+++ src/main/java/net/wasdev/websocket/EchoEncoderEndpoint.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package net.wasdev.websocket;
 
 import java.io.IOException;
@@ -14,19 +29,19 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint(value = "/EchoEncoderEndpoint", 
+@ServerEndpoint(value = "/EchoEncoderEndpoint",
                 decoders = EchoDecoder.class,
                 encoders = EchoEncoder.class)
 public class EchoEncoderEndpoint {
-    
+
     @OnOpen
     public void onOpen(Session session, EndpointConfig ec) {
     }
-    
+
     @OnClose
     public void onClose(Session session, CloseReason reason) {
     }
-    
+
     @OnMessage
     public void receiveMessage(EchoObject o, Session session) throws IOException, EncodeException {
         if (o.stopRequest() ) {
@@ -37,7 +52,7 @@ public class EchoEncoderEndpoint {
             }
         }
     }
-    
+
     @OnError
     public void onError(Throwable t) {
     }
